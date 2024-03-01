a4ff2201d62393f1528b0137f02d0f508575a419
Henrique Caltram <henrivai@hotmail.com>
2024-03-01 14:52:53 -0300

    Implement b_search method

---
+++ lib/searches.rb
@@ -0,0 +1,37 @@
+module Searches
+  def self.included(base)
+    base.class_eval do
+      attr_reader :list
+
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def b_search(element)
+      i = 0
+      j = list.length - 1
+      iterations = 0
+
+      while i <= j
+        iterations += 1
+        middle = (i + j) / 2
+
+        if element == list[middle]
+          puts "Element found in position #{middle}.\n"
+          puts "#{iterations} iterations needed.\n"
+
+          return middle
+        end
+
+        if element < list[middle]
+          j = middle - 1
+        else
+          i = middle + 1
+        end
+      end
+
+      puts "Element not present in list."
+    end
+  end
+end
