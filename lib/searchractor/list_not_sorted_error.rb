ac0a2a7c511b1daa5521648dfff37822f6185194
Henrique Caltram <henrivai@hotmail.com>
2024-03-29 09:51:07 -0300

    Add docs for Searchractor::ListNotSortedError and use #message instead of #initialize

--- lib/searchractor/list_not_sorted_error.rb
+++ lib/searchractor/list_not_sorted_error.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
+# Internal: Namespace module dedicated to ListNotSortedError class
 module Searchractor
+  # Internal: Searchractor's custom ListNotSortedError class
   class ListNotSortedError < StandardError
-    def initialize(msg='The list of elements is not sorted.')
-      super
+    # Internal: Overrides StandardError#message to return
+    #  class specific error message when raised.
+    def message
+      'The list of elements is not sorted.'
     end
   end
 end
